20b41623f2711ae6370e355892d3432d4390d3cf
Daniel Sanchez <eliasdanielsanchezlopez@gmail.com>
2020-09-03 11:10:51 -0500

    se añadió section advanced statics

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,12 @@
 let isMenuOpen = false;
 function showMenu(){
     let menumorado= document.getElementById('menumorado');
-    let sButton = document.getElementById('signButton');
     if(!isMenuOpen){
-        menumorado.style.height = "auto";
+        menumorado.style.display = "block";
         menumorado.style.padding= "15px";
-        sButton.style.display= "inline-block";
     }else{
-        menumorado.style.height = "0px";
+        menumorado.style.display = "none";
         menumorado.style.padding= "0px";
-        sButton.style.display= "none";
     }
     isMenuOpen=!isMenuOpen;
 }
