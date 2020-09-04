db5ecd2c292c2311a74eae53480e55715476c86b
alvarezohc <oscarhiramalvarezcampos@MacBook-Pro-de-Oscar.local>
2020-09-04 13:26:26 -0500

    mobile view finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
 let isMenuOpen = false;
 function showMenu(){
     let menumorado= document.getElementById('menumorado');
+    let sButton = document.getElementById('signButton');
     if(!isMenuOpen){
         menumorado.style.display = "block";
         menumorado.style.padding= "15px";
+        sButton.style.display= "inline-block";
     }else{
         menumorado.style.display = "none";
         menumorado.style.padding= "0px";
+        sButton.style.display= "none";
     }
     isMenuOpen=!isMenuOpen;
 }
